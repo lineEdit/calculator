2bd3eac1bc4ad5ba040cd7f47b1df43b4f8e46e6
lineedit <lineedit@gmail.com>
2020-04-01 09:31:48 +0500

    Stage 2, end.

--- Smart Calculator/task/src/calculator/Main.java
+++ Smart Calculator/task/src/calculator/Main.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -7,8 +8,28 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // put your code here
-        int x = scanner.nextInt();
-        int y = scanner.nextInt();
-        System.out.println(x + y);
+        while (scanner.hasNextLine()) {
+            String string = scanner.nextLine();
+            if (string.contains("/exit")) {
+                System.out.println("Bye!");
+                break;
+            }
+            if (string.isEmpty()) {
+                continue;
+            }
+            String[] stringSplit = string.split("\\s+");
+            int[] ints = new int[stringSplit.length];
+            for (int i = 0; i < stringSplit.length; ++i) {
+                ints[i] = Integer.parseInt(stringSplit[i]);
+            }
+            switch (ints.length) {
+                case 1:
+                    System.out.println(ints[0]);
+                    break;
+                case 2:
+                    System.out.println(ints[0] + ints[1]);
+                    break;
+            }
+        }
     }
 }
